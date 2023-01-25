231d91f070bae4479d262f64ba00124fc214d794
LukasJeahyoungLee <122358631+LukasJeahyoungLee@users.noreply.github.com>
2023-01-25 13:15:30 +0900

    python class 12

--- Basic/test3.py
+++ Basic/test3.py
@@ -1,2 +1,30 @@
 # 리스트 만들기
 li=[]
+li=list()
+
+
+
+
+
+
+#리스트 인덱스
+li=['a','b','c']
+
+li[0]
+li[1]
+li[2]
+
+li='d'
+
+
+#리스트 활용
+
+li=['a','b','c','d','e']
+
+li.index('c')        #위치 찾기
+
+li.append('f')       #추가하기1
+li.insert(0,'aa')    #추가하기2
+
+li.remove('aa')
+del li[2]
